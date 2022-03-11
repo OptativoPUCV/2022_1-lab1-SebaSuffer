823899e5858a8c6a165772e5f525247ca05f62f2
Your Name <you@example.com>
2022-03-11 19:44:27 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -12,12 +12,7 @@ y en c almacena el valor de la suma de a más b.
 */
 
 void suma(int a, int b, int * c) {
-   int a, b, c;
-   size_t i;
-
-   c = a + b;
-   printf("%i", c);
-   return 0;
+   *c = a + b;
 }
 
 /*

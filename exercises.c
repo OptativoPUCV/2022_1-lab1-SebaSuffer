1693ec78af21ac11b540aa67136ca08936faf222
Your Name <you@example.com>
2022-03-13 21:52:42 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -101,12 +101,7 @@ la cual retorna el valor en la posición i del vector v.
 */
 
 int obtenerValor(Vector * v, int i) {
-   /*
-   int valor;
-   valor = v[i];
-   return valor;
-   */
-  return 0;
+   return  v->datos[i];
 }
 
 /*
